42d976e6d4969efb022605815ae2534530a0f740
jasbatr1 <jaspalkaurbatra95@gmail.com>
2019-01-24 18:13:39 +0530

    pom files resolved

--- eureka-service/src/main/java/com/stackroute/eureka/EurekaApplication.java
+++ eureka-service/src/main/java/com/stackroute/eureka/EurekaApplication.java
@@ -1,15 +1,15 @@
-package com.stackroute.eurekaservice;
+package com.stackroute.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
-public class EurekaServiceApplication {
+@EnableEurekaServer
+public class EurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServiceApplication.class, args);
+		SpringApplication.run(EurekaApplication.class, args);
 	}
 
 }
--- zuul-service/src/main/java/com/stackroute/zuulservice/filter/ErrorFilter.java
+++ zuul-service/src/main/java/com/stackroute/zuulservice/filter/ErrorFilter.java
@@ -2,18 +2,16 @@ package com.stackroute.zuulservice.filter;
 
 import com.netflix.zuul.ZuulFilter;
 
-public class PostFilter extends ZuulFilter
-{
+public class ErrorFilter extends ZuulFilter {
 
     @Override
     public String filterType()
     {
-        return "post";
+        return "error";
     }
 
     @Override
-    public int filterOrder()
-    {
+    public int filterOrder() {
         return 1;
     }
 
@@ -25,7 +23,7 @@ public class PostFilter extends ZuulFilter
 
     @Override
     public Object run() {
-        System.out.println("Inside Response Filter");
+        System.out.println("Inside Route Filter");
 
         return null;
     }
